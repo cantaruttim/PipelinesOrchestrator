75e18365e63b21f76be0dbcfe9a064a907d49ac9
cantaruttim <cantaruttim@outlook.com>
2026-09-19 14:17:02 -0300

    first commit

---
+++ backend/dataforge-api/src/main/java/br/cantaruttim/dataforge_api/models/users/User.java
@@ -0,0 +1,58 @@
+package br.cantaruttim.dataforge_api.models.users;
+
+import java.util.UUID;
+
+public class User {
+    
+    private UUID id;
+    private String userName;
+    private String userEmail;
+    private boolean activate;
+
+    public User(UUID id, String userName, String userEmail, boolean activate) {
+        this.id = id;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.activate = true;
+    }
+
+    public User() {}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public boolean isActivate() {
+        return activate;
+    }
+
+    public void setActivate(boolean activate) {
+        this.activate = activate;
+    }
+
+    public void deactivateUser() {
+        this.activate = false;
+    }
+    
+    
+}
---
+++ backend/dataforge-api/src/test/java/br/cantaruttim/dataforge_api/DataforgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cantaruttim.dataforge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataforgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
